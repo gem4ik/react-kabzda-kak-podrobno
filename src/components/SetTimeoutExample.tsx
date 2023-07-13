8b8a1c2e2ec44316651bf9e8d9b33b62d955e527
gem4ik <mgemskij@gmail.com>
2023-07-13 13:43:00 +0300

    useEffect, setInterval, setTimeout

--- src/components/SetTimeoutExample.tsx
+++ src/components/SetTimeoutExample.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
+import './SetTimeoutExample.css'
 
 export const SetTimeoutExample = () => {
     const [fake, setFake] = useState(1)
     const [count, setCount] = useState(1)
-
+    const [ViewMode, setViewMode] = useState(false)
 
     // useEffect(() => {
     //     console.log('sdfhgb')
@@ -16,18 +17,39 @@ export const SetTimeoutExample = () => {
     const [time, setTime] = useState(realTime)
 
     useEffect(()=>{
-
         setInterval(()=> {
             setTime(new Date())
         }, 1000)
-
     }, [])
+
+    const secondsStyle = {
+        transform: `rotate(${time.getSeconds() * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${time.getMinutes() * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${time.getHours() * 30}deg)`
+    };
+
+    let switchClock = ViewMode
+        ?<div>
+            {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+        </div>
+        :<div>
+            <div className={"clock"}>
+                <div className={"analog-clock"}>
+                    <div className={"dial seconds"} style={secondsStyle} />
+                    <div className={"dial minutes"} style={minutesStyle} />
+                    <div className={"dial hours"} style={hoursStyle} />
+                </div>
+            </div>
+        </div>
 
     return (
         <div>
-            <div>
-                {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
-            </div>
+            {switchClock}
+            <button onClick={()=>setViewMode(!ViewMode)}>ViewMode</button>
             Hello,{count},{fake}
             <button onClick={() => setCount(count + 1)}> counter +</button>
             <button onClick={() => setFake(fake + 1)}> fake +</button>
